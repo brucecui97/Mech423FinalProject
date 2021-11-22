f516752b5fa716e157e79c25077b1134a63c0a16
bruce <brucec@hotmail.com>
2021-11-22 10:49:10 -0800

    only have verify running now

--- FInalProject/Program.cs
+++ FInalProject/Program.cs
@@ -47,30 +47,43 @@ namespace FInalProject
             // </snippet_client>
 
             // Detect - get features from faces.
-            DetectFaceExtract(client, IMAGE_BASE_URL, RECOGNITION_MODEL4).Wait();
+
+            //DetectFaceExtract(client, IMAGE_BASE_URL, RECOGNITION_MODEL4).Wait();
+
             // Find Similar - find a similar face from a list of faces.
-            FindSimilar(client, IMAGE_BASE_URL, RECOGNITION_MODEL4).Wait();
+
+            //FindSimilar(client, IMAGE_BASE_URL, RECOGNITION_MODEL4).Wait();
+
             // Verify - compare two images if the same person or not.
+
             Verify(client, IMAGE_BASE_URL, RECOGNITION_MODEL4).Wait();
 
             // Identify - recognize a face(s) in a person group (a person group is created in this example).
-            IdentifyInPersonGroup(client, IMAGE_BASE_URL, RECOGNITION_MODEL4).Wait();
+            
+            //IdentifyInPersonGroup(client, IMAGE_BASE_URL, RECOGNITION_MODEL4).Wait();
+            
             // LargePersonGroup - create, then get data.
-            LargePersonGroup(client, IMAGE_BASE_URL, RECOGNITION_MODEL4).Wait();
+            
+            //LargePersonGroup(client, IMAGE_BASE_URL, RECOGNITION_MODEL4).Wait();
+            
             // Group faces - automatically group similar faces.
-            Group(client, IMAGE_BASE_URL, RECOGNITION_MODEL4).Wait();
+            
+            //Group(client, IMAGE_BASE_URL, RECOGNITION_MODEL4).Wait();
+            
             // FaceList - create a face list, then get data
             // </snippet_maincalls>
 
-            FaceListOperations(client, IMAGE_BASE_URL).Wait();
+            //FaceListOperations(client, IMAGE_BASE_URL).Wait();
+            
             // Large FaceList - create a large face list, then get data
-            LargeFaceListOperations(client, IMAGE_BASE_URL).Wait();
+            
+            //LargeFaceListOperations(client, IMAGE_BASE_URL).Wait();
 
             // <snippet_persongroup_delete>
             // At end, delete person groups in both regions (since testing only)
             Console.WriteLine("========DELETE PERSON GROUP========");
             Console.WriteLine();
-            DeletePersonGroup(client, personGroupId).Wait();
+            ///DeletePersonGroup(client, personGroupId).Wait();
             // </snippet_persongroup_delete>
 
             Console.WriteLine("End of quickstart.");
